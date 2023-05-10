d7fb76f887db5f240874c81e5d6c4923ccd3d2bf
Esteban Cortina <estebannsebastian@gmail.com>
2023-05-09 23:15:52 -0600

    V2

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+const path = require('path');
+const staticPath = path.join(__dirname, "./public");
+app.use(express.static(staticPath));
+
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://test.mosquitto.org');
 
@@ -10,13 +15,14 @@ client.on('connect', () => {
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index3.html');
 })
 
-app.post('/', (req, res) => {
+app.get('/publish', (req, res) => {
   client.publish('focoEsli', 'Hola ESLI');
+  console.log('publish');
   res.status(200);
-  res.redirect('/');
+  res.json("Msj")
 })
 
 
